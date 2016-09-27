94a83ce9df7412581ad3306998b9afbe6a23ccea
Royal Pinto <royalpinto@gmail.com>
2016-09-27 22:09:04 +0530

    Added paginate middleware.

--- app/middlewares.js
+++ app/middlewares.js
@@ -5,6 +5,20 @@ const url = require('url');
 const querystring = require('querystring');
 
 
+const _parseNumber = (value, defaultValue, max) => {
+    if (value instanceof Array) {
+        value = value[0];
+    }
+
+    value = parseInt(value, 10);
+    value = value ? value : defaultValue;
+    if (max && value > max) {
+        value = max;
+    }
+    return value;
+};
+
+
 module.exports = {
     log: (req, res, next) => {
         res.on('finish', () => {
@@ -25,5 +39,14 @@ module.exports = {
     querystring: (req, res, next) => {
         req.query = querystring.parse(url.parse(req.url).query);
         next();
+    },
+
+    paginate: (defaultLimit, maxLimit) => {
+        return (req, res, next) => {
+            req.query.limit = _parseNumber(req.query.limit,
+                defaultLimit, maxLimit);
+            req.query.skip = _parseNumber(req.query.skip, 0);
+            next();
+        };
     },
 };
