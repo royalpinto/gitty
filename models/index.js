88ae6ab14e38e0de0ccec7617194e83e2cb37068
Royal Pinto <royalpinto@gmail.com>
2016-09-27 19:17:21 +0530

    Added initial models module.

---
+++ models/index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+
+const mongodb = require('mongodb');
+const config = require('./../config');
+
+
+let dbPromise;
+const init = () => {
+    if (!dbPromise) {
+        dbPromise = mongodb.MongoClient.connect(config.db.uri);
+    }
+    // This promise can be used here to initialize models.
+    // But for this assessment, not using any models.
+};
+
+init();
+
+
+const models = {
+
+    collection: name => {
+        // Init only for the first time.
+        if (!dbPromise) {
+            models.init();
+        }
+
+        return dbPromise
+        .then(db => {
+            return db.collection(name);
+        })
+        ;
+    },
+
+};
+
+
+module.exports = models;
