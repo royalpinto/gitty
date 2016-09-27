b29e7bb863955c57557c0e6bf97368588875802b
Royal Pinto <royalpinto@gmail.com>
2016-09-27 20:40:59 +0530

    Linked events service route to the app.

--- app/index.js
+++ app/index.js
@@ -1,10 +1,13 @@
 const http = require('http');
 const Router = require('./router');
 const middlewares = require('./middlewares');
+const events = require('./../routes/events');
 
 const router = new Router();
 
 router.use(/(?:)/, middlewares.log);
+router.use(/^\/api\/events\//, events);
+
 router.use(/(?:)/, (req, res) => {
     res.end('Hi');
 });
