2a6601df67b38cf40aefedb0c6978839fb55b41b
Royal Pinto <royalpinto@gmail.com>
2016-09-28 12:17:26 +0530

    Updated events service route use events controller.

--- routes/events.js
+++ routes/events.js
@@ -1,9 +1,19 @@
+'use strict';
+
+
 const Router = require('./../app/router');
 const router = new Router();
+const Controller = require('./../controllers/event');
 
 
 router.get(/^\/api\/events\//, (req, res, next) => {
-    res.end('Events Get');
+    let controller = new Controller();
+    let query = req.query;
+    controller.get(query.filter, query.limit, query.skip, query.order)
+    .then(data => {
+        res.json(data);
+    })
+    ;
 });
 
 
