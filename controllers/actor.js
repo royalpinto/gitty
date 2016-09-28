c2f2598eaa385cb5f01697a5f84be5d9c036657c
Royal Pinto <royalpinto@gmail.com>
2016-09-28 16:24:31 +0530

    Added getTopRepo method to actor controller.

--- controllers/actor.js
+++ controllers/actor.js
@@ -60,6 +60,47 @@ class ActorController extends Controller {
         ;
     }
 
+    // Fetch Top repository of an actor by number of events and created dates.
+    getTopRepo(id) {
+        return models
+        .collection(collectionName)
+        .then(collection => {
+            return new Promise((resolve, reject) => {
+                collection
+                .aggregate([
+                    // Take only given actors activities.
+                    {$match: {'actor.login': id}},
+
+                    // Group by repo and take the following:
+                    // - count(number of activities).
+                    // - createdAt.
+                    {$group: {
+                        _id: '$repo.id',
+                        createdAt: {$first: '$created_at'},
+                        repo: {$first: '$repo'},
+                        count: {$sum: 1},
+                    }},
+                    // Sort by desc count and then desc createdAt.
+                    {$sort: {count: -1, createdAt: -1}},
+
+                    // Limit by one.
+                    {$limit: 1},
+
+                    // Knock off unwanted fields.
+                    {$project: {_id: 0, repo: 1}},
+                ], (err, result) => {
+                    if (err) {
+                        return reject(err);
+                    }
+                    let doc = result[0];
+                    resolve(doc ? doc.repo : null);
+                })
+                ;
+            });
+        })
+        ;
+    }
+
 }
 
 
