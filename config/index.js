2747955e938763ca6d3d023cc7795daadef56b0d
Royal Pinto <royalpinto@gmail.com>
2016-09-29 13:59:03 +0530

    Updated config to use `const` than `var` for constants.

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
-var env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'development';
 
-var config = require("./" + env);
+const config = require("./" + env);
 config.env = env;
 
 module.exports = config;
