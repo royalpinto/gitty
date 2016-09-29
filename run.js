de50526c601067d2e2e90c8fc9b072102434aa1b
Royal Pinto <royalpinto@gmail.com>
2016-09-29 14:10:17 +0530

    Updated run.js to console the environment getting used for the app.

--- run.js
+++ run.js
@@ -2,6 +2,8 @@ const app = require('./app');
 const config = require('./config');
 
 
+console.log("Using %s environment.", config.env);
+
 app.on('error', console.error);
 app.on('listening', () => {
     console.log('Listening on %s', config.server.port);
