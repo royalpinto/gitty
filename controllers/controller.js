bf4d34adc7206dbd575e7f876f7fb4eda3cb314d
Royal Pinto <royalpinto@gmail.com>
2016-09-29 14:02:21 +0530

    Updated base controller class doc.

--- controllers/controller.js
+++ controllers/controller.js
@@ -5,6 +5,7 @@
  * @class
  * @classdesc An abstract base class for all controllers with high level
  * interface methods which have to be implemented by the sub classes.
+ * NOTE: It's just an empty base class for this demonstration.
  */
 class Controller {
 
