e90a2808cbf3619d53b84b50ff7f2ad806d02574
Royal Pinto <royalpinto@gmail.com>
2016-09-29 14:30:44 +0530

    Updated models module doc.

--- models/index.js
+++ models/index.js
@@ -9,6 +9,9 @@ let dbPromise;
 const init = () => {
     dbPromise = mongodb.MongoClient.connect(config.db.uri);
     // This promise can be used here to initialize models.
+    // Ideally this would initialize models and
+    // later models can be accessed using syntax: models.<ModelName>
+    // Ex: models.User
     // But for this assessment, not using any models.
 };
 
