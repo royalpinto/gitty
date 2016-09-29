8594dba2c51224068003ac6dd446fbfe0d094806
Royal Pinto <royalpinto@gmail.com>
2016-09-29 14:23:18 +0530

    Updated routes to initialize controller only once and not for every request.

--- routes/actors.js
+++ routes/actors.js
@@ -5,11 +5,11 @@ const errors = require('./../errors');
 const Router = require('./../app/router');
 const router = new Router();
 const Controller = require('./../controllers/actor');
+const controller = new Controller();
 
 
 router.get(/^\/api\/actors\/(\w+)\/repos\/top\/$/, (req, res, next) => {
     let id = req.params[0];
-    let controller = new Controller();
     controller.getTopRepo(id)
     .then(data => {
         res.json(data);
@@ -23,7 +23,6 @@ router.get(/^\/api\/actors\/(\w+)\/repos\/top\/$/, (req, res, next) => {
 
 router.get(/^\/api\/actors\/(\w+)\/$/, (req, res, next) => {
     let id = req.params[0];
-    let controller = new Controller();
     controller.getById(id)
     .then(data => {
         res.json(data);
@@ -37,7 +36,6 @@ router.get(/^\/api\/actors\/(\w+)\/$/, (req, res, next) => {
 
 router.delete(/^\/api\/actors\/(\w+)\/events\/$/, (req, res, next) => {
     let id = req.params[0];
-    let controller = new Controller();
     controller.deleteEvents(id)
     .then(() => {
         res.status(204).end();
--- routes/repos.js
+++ routes/repos.js
@@ -5,10 +5,10 @@ const errors = require('./../errors');
 const Router = require('./../app/router');
 const router = new Router();
 const Controller = require('./../controllers/repo');
+const controller = new Controller();
 
 
 router.get(/^\/api\/repos\//, (req, res, next) => {
-    let controller = new Controller();
     let query = req.query;
     controller.get(query.filter, query.limit, query.skip, query.order)
     .then(data => {
